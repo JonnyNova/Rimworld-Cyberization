20592a68346df9ca32c9fe374ad15bf0153469d8
JonnyNova <JonnyNova@users.noreply.github.com>
2019-05-15 18:15:59 -0400

    Setup solution

---
+++ Source/Cyberization/Mod.cs
@@ -0,0 +1,12 @@
+using Verse;
+
+namespace FrontierDevelopments.Cyberization
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            
+        }
+    }
+}
