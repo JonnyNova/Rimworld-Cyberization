1f1f97594aad42cf85a8cb3bd7b8e676f617b8b2
JonnyNova <JonnyNova@users.noreply.github.com>
2019-05-17 22:11:13 -0400

    Add a power belt and trigger energy need display from apparel. Part of #10

---
+++ Source/Cyberization/Power/PartEnergyNeed.cs
@@ -0,0 +1,89 @@
+using System.Linq;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace FrontierDevelopments.Cyberization.Power
+{
+    public class PartEnergyNeed : Need
+    {
+        public PartEnergyNeed(Pawn pawn) : base(pawn)
+        {
+        }
+        
+        public override float MaxLevel
+        {
+            get
+            {
+                var total = PowerProvider.Providers(pawn).Aggregate(0L, (sum, provider) => sum + provider.MaxEnergy);
+                if (total <= 0) return 1f;
+                return total;
+            }
+        }
+        
+        public override float CurLevel => 
+            PowerProvider.Providers(pawn).Aggregate(0L, (sum, provider) => sum + provider.Energy);
+
+        public override void NeedInterval()
+        {
+        }
+    }
+
+    [DefOf]
+    public static class PartEnergyNeedDefOf
+    {
+        public static NeedDef PartEnergy;
+
+        static PartEnergyNeedDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof (NeedDefOf));
+        }
+    }
+
+    static class PartEnergyNeedFromApparel
+    {
+        private static bool ApparelHasSource(Apparel apparel)
+        {
+            return apparel.AllComps.OfType<IPowerProvider>().Any();
+        }
+
+        private static void HandleApparelChange(Apparel apparel, Pawn pawn)
+        {
+            if (ApparelHasSource(apparel))
+            {
+                pawn.needs.AddOrRemoveNeedsAsAppropriate();
+            }
+        }
+
+        [HarmonyPatch(typeof(Pawn_ApparelTracker), nameof(Pawn_ApparelTracker.Notify_ApparelAdded))]
+        static class ApparelAdded
+        {
+            [HarmonyPostfix]
+            static void OnAdded(Pawn_ApparelTracker __instance, Apparel apparel)
+            {
+                HandleApparelChange(apparel, __instance.pawn);
+            }
+        }
+        
+        [HarmonyPatch(typeof(Pawn_ApparelTracker), nameof(Pawn_ApparelTracker.Notify_ApparelRemoved))]
+        static class ApparelRemoved
+        {
+            [HarmonyPostfix]
+            static void OnRemoved(Pawn_ApparelTracker __instance, Apparel apparel)
+            {
+                HandleApparelChange(apparel, __instance.pawn);
+            }
+        }
+
+        [HarmonyPatch(typeof(Pawn_NeedsTracker), "ShouldHaveNeed")]
+        static class ShouldHaveNeed
+        {
+            [HarmonyPostfix]
+            static bool AddEnergySourceCheck(bool __result, Pawn_NeedsTracker __instance, NeedDef nd, Pawn ___pawn)
+            {
+                if (nd != PartEnergyNeedDefOf.PartEnergy) return __result;
+                return PowerProvider.Providers(___pawn).Any() || PowerConsumer.All(___pawn).Any();
+            }
+        }
+    }
+}
